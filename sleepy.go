781ece0c4611896cd99d2ab077709a8faaf59062
Bryan Garza <brygarza@gmail.com>
2014-09-10 15:19:11 -0700

    Define Resource type

--- sleepy.go
+++ sleepy.go
@@ -1,6 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"net/url"
+)
+
+type Resource interface {
+	Get(values url.Values) (int, interface{})
+	Post(values url.Values) (int, interface{})
+	Put(values url.Values) (int, interface{})
+	Delete(values url.Values) (int, interface{})
+}
 
 func response(rw http.ResponseWriter, request *http.Request) {
 	rw.Write([]byte("Hello world."))
